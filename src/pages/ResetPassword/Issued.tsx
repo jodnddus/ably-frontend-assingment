a5b32037dedcbbfe82054055a841211ecb3a8b1e
jodnddus <lockhost23@gmail.com>
2022-03-31 01:56:17 +0900

    인증 코드 발급 요청 페이지 UI 구성, 동작 구현 완료

--- src/pages/ResetPassword/Issued.tsx
+++ src/pages/ResetPassword/Issued.tsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+import { Button, Input } from "components";
+
+import { setAuthCode } from "reducers/auth";
+
+import { useAppDispatch } from "hooks";
+
+import { getAuthCodeForResetPassword } from "lib/services/auth";
 
 function Issued() {
-  return <IssuedContainer>issued</IssuedContainer>;
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
+
+  const dispatch = useAppDispatch();
+
+  const onClickNext = () => {
+    getAuthCodeForResetPassword(email)
+      .then((res) => {
+        dispatch(setAuthCode(res));
+        navigate("/resetPassword/validation");
+      })
+      .catch((e) => window.alert(e.response.data.error.message));
+  };
+
+  return (
+    <IssuedContainer>
+      <h1>인증 코드 발급 요청</h1>
+      <Input
+        type="text"
+        value={email}
+        onChange={(value) => setEmail(value)}
+        isError={false}
+      />
+      <Button
+        type="button"
+        name="다음"
+        isDisable={false}
+        onClick={onClickNext}
+      />
+    </IssuedContainer>
+  );
 }
 
 const IssuedContainer = styled.div``;
