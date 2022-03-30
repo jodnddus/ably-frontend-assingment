18cfc34abf21282d1d2518bb995f63ecdd12294b
jodnddus <lockhost23@gmail.com>
2022-03-31 02:19:14 +0900

    인증 코드 검증 api 함수의 응답값 타입 추가

--- src/lib/services/auth/validationAuthCode.ts
+++ src/lib/services/auth/validationAuthCode.ts
@@ -1,15 +1,20 @@
 import { auth } from "lib/apis";
 
+import { AuthCodeValidationTokenType } from "lib/types/auth.types";
+
 const validationAuthCode = async (
   email: string,
   authCode: string,
   issueToken: string
 ) => {
-  const { data } = await auth.post("/api/reset-password", {
-    email,
-    authCode,
-    issueToken,
-  });
+  const { data } = await auth.post<AuthCodeValidationTokenType>(
+    "/api/reset-password",
+    {
+      email,
+      authCode,
+      issueToken,
+    }
+  );
 
   return data;
 };
