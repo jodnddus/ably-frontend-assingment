edda64ff6f924b58aeed496395371cdcd38e37c6
jodnddus <lockhost23@gmail.com>
2022-03-30 14:49:19 +0900

    user 리듀서의 initialState에 타입 추가

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -1,10 +1,20 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+import { UserInfoType } from "lib/types/auth.types";
+
+interface InitialStateType {
+  accessToken: string;
+  userInfo: UserInfoType | null;
+}
+
+const initialState: InitialStateType = {
+  accessToken: "",
+  userInfo: null,
+};
+
 export const userSlice = createSlice({
   name: "user",
-  initialState: {
-    accessToken: "",
-  },
+  initialState,
   reducers: {
     setAccessToken(state, action: PayloadAction<string>) {
       state.accessToken = action.payload;
