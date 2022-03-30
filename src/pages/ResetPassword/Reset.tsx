33ca9b294aa5a809d98f728b29f84fb011764499
jodnddus <lockhost23@gmail.com>
2022-03-31 02:47:06 +0900

    비밀번호 변경 페이지 UI 구성

--- src/pages/ResetPassword/Reset.tsx
+++ src/pages/ResetPassword/Reset.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { Button, Input } from "components";
 
 function Reset() {
-  return <ResetContainer>reset</ResetContainer>;
+  const [newPassword, setNewPassword] = useState("");
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
+
+  return (
+    <ResetContainer>
+      <h1>비밀번호 변경</h1>
+      <Input
+        type="password"
+        value={newPassword}
+        isError={false}
+        onChange={(value) => setNewPassword(value)}
+      />
+      <Input
+        type="password"
+        value={newPasswordConfirm}
+        isError={false}
+        onChange={(value) => setNewPasswordConfirm(value)}
+      />
+      <Button type="button" name="다음" isDisable={false} onClick={() => {}} />
+    </ResetContainer>
+  );
 }
 
 const ResetContainer = styled.div``;
