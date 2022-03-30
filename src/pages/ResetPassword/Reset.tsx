76c9859fa2ec8ebb72d870cc8d3ad54d946e5221
jodnddus <lockhost23@gmail.com>
2022-03-31 02:48:04 +0900

    비밀번호 변경 페이지 동작 구현

--- src/pages/ResetPassword/Reset.tsx
+++ src/pages/ResetPassword/Reset.tsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
 import { Button, Input } from "components";
 
+import { useAppSelector } from "hooks";
+
+import { resetPassword } from "lib/services/auth";
+
 function Reset() {
+  const auth = useAppSelector((state) => state.auth);
+
   const [newPassword, setNewPassword] = useState("");
   const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
+
+  const navigate = useNavigate();
+
+  const onClickNext = () => {
+    resetPassword(
+      auth.email,
+      auth.confirmToken,
+      newPassword,
+      newPasswordConfirm
+    )
+      .then(() => {
+        navigate("/");
+      })
+      .catch((e) => window.alert(e.response.data.error.message));
+  };
 
   return (
     <ResetContainer>
@@ -21,7 +44,12 @@ function Reset() {
         isError={false}
         onChange={(value) => setNewPasswordConfirm(value)}
       />
-      <Button type="button" name="다음" isDisable={false} onClick={() => {}} />
+      <Button
+        type="button"
+        name="다음"
+        isDisable={false}
+        onClick={onClickNext}
+      />
     </ResetContainer>
   );
 }
