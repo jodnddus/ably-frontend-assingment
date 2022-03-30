612d764bc4357b4a3f35a63d30b5582bed154f3b
jodnddus <lockhost23@gmail.com>
2022-03-30 14:44:49 +0900

    UserInfoType 수정: 백엔드 문서에는 없지만 password키가 들어오는 것을 확인

--- src/lib/types/auth.types.ts
+++ src/lib/types/auth.types.ts
@@ -9,6 +9,7 @@ interface LogoutResponseType {
 interface UserInfoType {
   name: string; // 이름
   email: string; // 이메일
+  password: string; // 비밀번호
   profileImage: string; // 프로필 사진 URL
   lastConnectedAt: Date; // 마지막 접속 일자
 }
