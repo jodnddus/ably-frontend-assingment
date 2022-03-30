ecf16aac3397294d59aa12c17326fdcd3c6b8ca8
jodnddus <lockhost23@gmail.com>
2022-03-30 12:07:46 +0900

    제작된 컴포넌트를 이용해 기본적인 로그인 폼 UI 구성

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+import { Button, Input } from "components";
+
 function Login() {
-  return <h1>로그인 페이지</h1>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit: ", e);
+  };
+
+  return (
+    <LoginContainer>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          <Input
+            type="text"
+            value={id}
+            isError={false}
+            onChange={(value) => setId(value)}
+          />
+          <Input
+            type="password"
+            value={password}
+            isError={false}
+            onChange={(value) => setPassword(value)}
+          />
+        </div>
+        <Button type={"submit"} name="버튼" isDisable={false} />
+      </form>
+    </LoginContainer>
+  );
 }
+
+const LoginContainer = styled.div`
+  display: flex;
+`;
 
 export default Login;
