9d923241baa9bc4da040a005de6939b2d8def763
jodnddus <lockhost23@gmail.com>
2022-03-30 15:01:32 +0900

    /userInfo 페이지 진입 시 유저 정보 가져오게 수정

--- src/pages/UserInfo/index.tsx
+++ src/pages/UserInfo/index.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 
+import { setUserInfo } from "reducers/user";
+
+import { useAppDispatch, useAppSelector } from "hooks";
+
+import { getUserInfo } from "lib/services/auth";
+
 function UserInfo() {
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    getUserInfo(user.accessToken)
+      .then((res) => dispatch(setUserInfo(res)))
+      .catch((e) => console.log(e.response));
+  }, [user.accessToken, dispatch]);
+
   return <UserInfoContainer></UserInfoContainer>;
 }
 
