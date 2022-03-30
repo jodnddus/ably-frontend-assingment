0630de2b0434128b77d55ca676553f49b61fba72
jodnddus <lockhost23@gmail.com>
2022-03-31 02:09:41 +0900

    인증 코드 검증 페이지 UI 구성, 타이머 데이터 연동

--- src/pages/ResetPassword/Validation.tsx
+++ src/pages/ResetPassword/Validation.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import styled from "styled-components";
 
+import { Button, Input, Timer } from "components";
+
+import { useAppSelector } from "hooks";
+
 function Validation() {
-  return <ValidationContainer>validation</ValidationContainer>;
+  const auth = useAppSelector((state) => state.auth);
+
+  const [authCode, setAuthCode] = useState("");
+
+  if (!auth.remainMillisecond) {
+    return null;
+  }
+
+  return (
+    <ValidationContainer>
+      <h1>인증 코드 검증</h1>
+      <Timer second={auth.remainMillisecond} />
+      <Input
+        type="text"
+        value={authCode}
+        onChange={(value) => setAuthCode(value)}
+        isError={false}
+      />
+      <Button type="button" name="다음" isDisable={false} onClick={() => {}} />
+    </ValidationContainer>
+  );
 }
 
 const ValidationContainer = styled.div``;
