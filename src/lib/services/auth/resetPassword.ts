6dd26565d0bf142f8adb8a2cb8f9748991dad501
jodnddus <lockhost23@gmail.com>
2022-03-31 02:43:33 +0900

    비밀번호 변경 api 함수 응답값 타입 추가

--- src/lib/services/auth/resetPassword.ts
+++ src/lib/services/auth/resetPassword.ts
@@ -1,17 +1,22 @@
 import { auth } from "lib/apis";
 
+import { ResetPasswordResponseType } from "lib/types/auth.types";
+
 const resetPassword = async (
   email: string,
   confirmToken: string,
   newPassword: string,
   newPasswordConfirm: string
 ) => {
-  const { data } = await auth.post("/api/reset-password", {
-    email,
-    confirmToken,
-    newPassword,
-    newPasswordConfirm,
-  });
+  const { data } = await auth.post<ResetPasswordResponseType>(
+    "/api/reset-password",
+    {
+      email,
+      confirmToken,
+      newPassword,
+      newPasswordConfirm,
+    }
+  );
 
   return data;
 };
