6aff0ecbf4ebcd621faf20329c581ded7929de41
jodnddus <lockhost23@gmail.com>
2022-03-31 13:42:32 +0900

    앱 전체 폭 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import styled from "styled-components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Login, ResetPassword, UserInfo } from "pages";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/userInfo" element={<UserInfo />} />
-        <Route path="/resetPassword/*" element={<ResetPassword />} />
-      </Routes>
-    </Router>
+    <AppContainer>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/userInfo" element={<UserInfo />} />
+          <Route path="/resetPassword/*" element={<ResetPassword />} />
+        </Routes>
+      </Router>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  margin: 0 auto;
+  min-width: 470px;
+  max-width: 640px;
+`;
 
 export default App;
