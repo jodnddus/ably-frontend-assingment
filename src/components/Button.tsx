d0df934d89c816ab5c9720f85863fa275f9773e8
jodnddus <lockhost23@gmail.com>
2022-03-31 13:21:59 +0900

    버튼 컴포넌트 디자인

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -31,6 +31,23 @@ function Button({
   );
 }
 
-const ButtonTag = styled.button<{ isDisable: boolean }>``;
+const ButtonTag = styled.button<{ isDisable: boolean }>`
+  color: black;
+  background-color: #f6f8fa;
+  border: 0;
+  width: 100%;
+  height: 54px;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+  border: 1px solid rgba(27 31 36 / 15%);
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #f3f4f6;
+  }
+  &:active {
+    background-color: #ebecf0;
+  }
+`;
 
 export default Button;
